870c8157f5ee27842494dd65fbc15b061d254ccd
Maddie Black <madalynh.black@gmail.com>
2021-12-07 15:17:07 -0500

    day one passing

--- solutions/2021/01/index.js
+++ solutions/2021/01/index.js
@@ -1,19 +1,18 @@
 import { example, data } from "./input";
 
-export const inputParser = (input) => input.split("\n");
+export const inputParser = (input) => input.split("\n").map((i) => parseInt(i));
 
 const parsedData = inputParser(data);
 
 export const part1 = (input = parsedData) => {
-  return parsedData.filter((d, i) => (i === 0 ? false : d > parsedData[i - 1]))
-    .length;
+  return input.filter((d, i) => (i === 0 ? false : d > input[i - 1])).length;
 };
 
 export const part2 = (input = parsedData) => {
   const windowIndexes = [0, 1, 2];
 
-  const windowSums = parsedData.reduce((acc, meas, i) => {
-    const window = windowIndexes.map((w) => parsedData[w + i]);
+  const windowSums = input.reduce((acc, meas, i) => {
+    const window = windowIndexes.map((w) => input[w + i]);
 
     if (window.includes(undefined)) return acc;
 
